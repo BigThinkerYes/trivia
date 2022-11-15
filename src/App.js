10e5d04c40deca2c0139ef0364c55e5522824d2c
BigThinkerYes <designaspnet4@hotmail.com>
2022-11-15 11:25:54 -0600

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import React, { Component } from 'react';
-import { Question } from './components';
+import React, { Component } from "react";
+import { Question } from "./components";
 
-const category = '';
+const category = "";
 const TRIVIA_API = `https://opentdb.com/api.php?amount=1&category=${category}&difficulty=easy`;
 
 class App extends Component {
-
-
+  constructor(props) {
+    super(props);
+    this.state = { question: null };
+  }
+  fetchApi = async () => {
+    try {
+      const res = await fetch(TRIVIA_API);
+      const data = await res.json();
+      console.log(data);
+      this.setState({ question: data.results[0] });
+    } catch (error) {
+      console.error(`error ${error} fetching TRIVIA_API`);
+    }
+  };
+  componentDidMount() {
+    this.fetchApi();
+  }
   render() {
     return (
-      <div className='container l:w-50 p-5'>
-        <h1 className='display-1'>Trivia</h1>
-        <h2 className='fw-lighter fs-5 mb-4'>
-          (we couldn&lsquo;t think of a better name,{' '}
-          <span className='fw-bolder'>sorry</span>)
+      <div className="container l:w-50 p-5">
+        <h1 className="display-1">Trivia</h1>
+        <h2 className="fw-lighter fs-5 mb-4">
+          (we couldn&lsquo;t think of a better name,{" "}
+          <span className="fw-bolder">sorry</span>)
         </h2>
         <hr />
+        {/* pass question to state */}
         <div>
           {/* Render question here */}
+          {this.state.question && <Question question={this.state.question} />}
         </div>
       </div>
     );
